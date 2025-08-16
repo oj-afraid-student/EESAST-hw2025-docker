26e6dd1d6460ccfd3a2cd3f8fcd24f335455d2e7
赵景言 <1373241747@qq.com>
2025-08-16 15:45:45 +0800

    docker_赵景言_笃实45

--- EESAST-hw2025-docker/hw_copy/run.sh
+++ EESAST-hw2025-docker/hw_copy/run.sh
@@ -1,1 +1,7 @@
-# Add your commands here
+#!/bin/bash
+# 构建Docker镜像（-t指定镜像名，.表示当前目录为构建上下文）
+docker build -t hw_copy_image -f hw_copy/Dockerfile .
+
+# 运行mul程序，传入参数3和4
+docker run --rm hw_copy_image /app/mul 3 4  # 运行mul程序，传入参数3和4
+#docker run --rm hw_copy_image /app/add 5 6  # 运行add程序，传入参数5和6
--- EESAST-hw2025-docker/hw_vol/run.sh
+++ EESAST-hw2025-docker/hw_vol/run.sh
@@ -1,1 +1,6 @@
-# Add your commands here
+docker build -t hw_vol_image -f hw_vol/Dockerfile .
+
+docker run --rm \
+  --mount type=bind,source=$(pwd)/src,target=/app \
+  hw_vol_image \
+  sh -c "g++ -o /app/mul /app/mul.cpp && /app/mul $1 $2"  # 编译并运行mul
